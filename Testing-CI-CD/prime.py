9d42e800ca9c5e31d78a8e0e25f164a16462196c
Abato <abatodearo@gmail.com>
2024-09-28 07:45:22 +0100

    Added new files

---
+++ Testing-CI-CD/prime.py
@@ -0,0 +1,10 @@
+import math
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % 1 == 0:
+            return False
+    else:
+        return True
